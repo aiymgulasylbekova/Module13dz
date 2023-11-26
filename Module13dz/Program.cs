eb91819472589f94c4714fe07bbf67e7b806ce39
aiymgulasylbekova <124632132+aiymgulasylbekova@users.noreply.github.com>
2023-11-26 23:46:26 +0600

    Добавьте файлы проекта.

---
+++ Module13dz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module13dz
+{
+    class Program
+    {
+        static void Main()
+        { 
+            Queue<Client> clientQueue = new Queue<Client>();
+
+            clientQueue.Enqueue(new Client(1, "Aiym", "Коллцентр"));
+            clientQueue.Enqueue(new Client(2, "Асыл", "Хелпдеск"));
+            clientQueue.Enqueue(new Client(3, "Ару", "Юр"));
+
+            while (clientQueue.Count > 0)
+            {
+                Client currentClient = clientQueue.Dequeue();
+                Console.WriteLine($"Обслуживаем клиента {currentClient.Name} (ID: {currentClient.Id}, Тип обслуживания: {currentClient.ServiceType})");
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
